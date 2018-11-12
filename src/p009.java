87dace172b9fa93f619b5f6144b1552b939676f9
zenazor13 <44451325+zenazor13@users.noreply.github.com>
2018-11-12 12:15:28 -0500

    updated p009

--- src/p009.java
+++ src/p009.java
@@ -11,6 +11,8 @@ public class p009 {
 
 	public static void main(String[] args) {
 
+		//old brute force solution -- avg solution time: 675ms
+		/*
 		for (int a = 1; a < 500; a++) {
 			for (int b = 1; b < 500; b++) {
 				for (int c = 1; c < 500; c++) {
@@ -19,6 +21,24 @@ public class p009 {
 						System.out.println(a * b * c);
 						System.exit(0);
 					}
+				}
+			}
+		}
+		*/
+
+		//streamlined solution using Euclid's formula -- avg solution time: <1ms
+
+		int a, b, c;
+		int m = 2, n;
+
+		for (n = 1; n < m; n++) {
+			for (m = 2; m < 25; m++) {
+				a = (m * m) - (n * n);
+				b = 2 * m * n;
+				c = (m * m) + (n * n);
+
+				if (a + b + c == 1000) {
+					System.out.println(a * b * c);
 				}
 			}
 		}
